ef6ddc3983889cff4f436fff34bb252201a818b6
Charles Felipe <charles.ffn3@gmail.com>
2021-02-22 16:34:45 -0300

    uploading exercise03&04

---
+++ exercise03.py
@@ -0,0 +1,5 @@
+print('Soma entre dois números.')
+x = int(input('Digite o primeiro número: '))
+y = int(input('Digite o segundo número: '))
+
+print('O resultado da soma entre {} e {} é {}.'.format(x, y, x+y))
