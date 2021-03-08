fbb008cb0af865a8e911b92fd0f812c3308c25c3
Charles Felipe <charles.ffn3@gmail.com>
2021-03-08 20:53:43 -0300

    Update exercise22

--- exercise22.py
+++ exercise22.py
@@ -1,10 +1,14 @@
-name = input('Digite seu nome completo: ')
+name = str(input('Digite seu nome completo: ')).strip()
 
 print(name.upper())
 print(name.lower())
 
-nospace = (name.replace(" ", ""))
-print(len(nospace))
+# Tambem poderia ser usado
+nospace = len(name) - name.count(' ')
+# nospace = (name.replace(" ", ""))
+print(nospace)
 
+# Tambem poderia ser usado
+# print(name.find(' '))
 dividido = name.split()
 print(len(dividido[0]))
