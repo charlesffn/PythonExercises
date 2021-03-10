0997ee07018571f903ab7203e6c0904d88d9a251
Charles Felipe <charles.ffn3@gmail.com>
2021-03-09 21:21:16 -0300

    conditions examples

---
+++ condicoes.py
@@ -0,0 +1,8 @@
+tempo = int(input('Quantos anos tem seu carro? '))
+if tempo <= 3:
+    print('Seu carro está novo!')
+else:
+    print('Voce precisa trocar de carro!')
+# Tambem pode ser feito --
+# print('Seu carro está novo!' if tempo <= 3 else 'Voce precisa trocar de carro!')
+print('--FIM--')
